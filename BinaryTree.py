034c181f4119c2801aad5bcc1450305a0979051d
Rob <robertjustinianpeng@gmail.com>
2014-12-10 19:26:35 -0600

    added print and search

--- BinaryTree.py
+++ BinaryTree.py
@@ -17,33 +17,20 @@ class BinaryTree(object):
 
 	def print_node(self, root):
 		if not root:
+			print("pass")
 			pass
 		else:
 			self.print_node(root.left)
 			print(root.cargo)
 			self.print_node(root.right)
 
-	def search(self, root, number):
-		if not root:
-			pass
-		else:
-			if root.cargo == number:
-				return "found"
-			elif root.cargo > number:
-				self.search(root.left, number)
-			else:
-				self.search(root.right, number)
-
-
-# a = BinaryTree(0)
-# a.root = Node(5)
-# a.add(a.root,3)
-# a.add(a.root,2)
-# a.add(a.root,1)
-# a.add(a.root,4)
-# a.add(a.root,5)
-# a.add(a.root,6)
-# a.add(a.root,7)
-# a.print_node(a.root)
-# a.search(a.root,5)
-# a.search(a.root,15)
+a = BinaryTree(0)
+a.root = Node(5)
+a.add(a.root,1)
+a.add(a.root,2)
+a.add(a.root,3)
+a.add(a.root,4)
+a.add(a.root,5)
+a.add(a.root,6)
+a.add(a.root,7)
+a.print_node(a.root)
